c3c668a5581da368d87a565cd439bd22b57c3ba8
andrew <me@andreww.co>
2023-08-27 08:39:53 -0500

    add old changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,10 @@ export default defineNuxtConfig({
 					href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
 					rel: "stylesheet",
 				},
+				{
+					href: "https://fonts.googleapis.com/css?family=Azeret+Mono:300,400,500,700&display=swap",
+					rel: "stylesheet",
+				},
 			],
 		},
 	},
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
 	<div class="flex items-center justify-center font-black h-screen flex-col bg-[#FF00FF]" >
+		<div :style="config.text ? '' : `margin-bottom: 68px;`">
+			<h1 v-if="config.text" class="mb-4 px-4 py-2 text-3xl font-semibold text-white bg-stone-700 border-white rounded-lg">{{config.text}}</h1>
+		</div>
 		<div class="flex rounded-lg">
 			<div class="ml-4 flex py-2 border-b-8 rounded-lg text-white" :style="`background-color: ${config.guestMainHex}; border-color: ${config.guestSecondaryHex}`">
 				<div class="my-auto ml-2 mt-1 block">
-					<div v-for="n in guest.timeouts" class="mb-1 rounded-sm px-0.5" :style="`background-color: ${config.guestSecondaryHex}`">&nbsp;</div>
+					<div v-for="n in guest.timeouts" class="mb-1 rounded-sm px-0.5 text-md" :style="`background-color: ${config.guestSecondaryHex}`">&nbsp;</div>
 				</div>
 				<div class="flex px-8">
-					<img :src="config.guestImgSource" class="my-auto h-16 w-14" />
+					<img :src="config.guestImgSource" class="mt-4 h-16 w-18" />
 					<div class="my-auto ml-10 flex">
 						<i class="fa-solid fa-circle my-auto mr-2 text-sm" :style="`color: ${possession === 'guest' ? config.guestSecondaryHex : config.guestMainHex}`"></i>
-						<h1 class="mb-1 text-6xl font-extrabold">{{guest.score}}</h1>
+						<h1 class="text-6xl font-extrabold">{{guest.score}}</h1>
 					</div>
 				</div>
 			</div>
@@ -19,21 +22,26 @@
 					<img :src="config.homeImgSource" class="my-auto h-16 w-12" />
 					<div class="my-auto ml-10 flex">
 						<i class="fa-solid fa-circle my-auto mr-2 text-sm" :style="`color: ${possession === 'home' ? config.homeSecondaryHex : config.homeMainHex}`"></i>
-						<h1 class="mb-1 text-6xl font-extrabold">{{home.score}}</h1>
+						<h1 class="text-6xl font-extrabold">{{home.score}}</h1>
 					</div>
 				</div>
 				<div class="my-auto mr-2 mt-1 block">
-					<div v-for="n in home.timeouts" class="mb-1 rounded-sm px-0.5" :style="`background-color: ${config.homeSecondaryHex}`">&nbsp;</div>
+					<div v-for="n in home.timeouts" class="mb-1 rounded-sm px-0.5 text-md" :style="`background-color: ${config.homeSecondaryHex}`">&nbsp;</div>
 				</div>
 			</div>
 
 			<div class="ml-4 rounded-lg flex bg-stone-700 px-8 py-6 my-auto text-white border-b-8 border-white">
-				<h1 class="text-5xl font-extrabold">Q{{quarter}}</h1>
+				<h1 class="text-6xl font-extrabold">Q{{quarter}}</h1>
 				<div class="ml-5 mr-5 rounded-sm bg-white px-0.5"></div>
-				<h1 class="text-5xl font-extrabold">{{formatTimeLeft}}</h1>
+				<h1 class="text-6xl font-extrabold">{{formatMinutes}}:{{formatSeconds}}</h1>
 				<div class="ml-5 mr-5 rounded-sm bg-white px-0.5"></div>
 				<div class="my-auto">
-					<h1 class="text-center text-5xl font-extrabold mb-2">{{formatDown}} & {{toGo}}</h1>
+					<div class="flex">
+						<h1 class="text-center text-6xl font-extrabold">{{formatDown}}</h1>
+						<h1 class="ml-4 mr-4 text-center text-6xl font-extrabold">&</h1>
+						<h1 class="text-center text-6xl font-extrabold">{{toGo}}</h1>
+					</div>
+					<!-- <h1 class="text-center text-6xl font-extrabold">{{formatDown}} & {{toGo}}</h1> -->
 				</div>
 			</div>
 		</div>
@@ -56,16 +64,8 @@ const quarter = ref("1")
 const timeLeft = ref("750")
 const down = ref("1")
 const toGo = ref("10")
-
-const formatTimeLeft = computed(() => {
-	const minutes = Math.floor(timeLeft.value / 60);
-	let seconds = timeLeft.value % 60;
-
-	if (seconds < 10) {
-		seconds = `0${seconds}`;
-	}
-	return `${minutes}:${seconds}`;
-})
+const minutes = ref("1")
+const seconds = ref("30")
 
 const formatDown = computed(() => {
 	switch (down.value) {
@@ -80,6 +80,22 @@ const formatDown = computed(() => {
 	}
 })
 
+const formatMinutes = computed(() => {
+	if (minutes.value < 10) {
+		return "0" + minutes.value
+	} else {
+		return minutes.value
+	}
+})
+
+const formatSeconds = computed(() => {
+	if (seconds.value < 10) {
+		return "0" + seconds.value
+	} else {
+		return seconds.value
+	}
+})
+
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
@@ -89,7 +105,9 @@ const subscribeToScoreboard = async () => {
     console.log(scoreboard);
     down.value = scoreboard.down
     toGo.value = scoreboard.yards_to_go
-    timeLeft.value = scoreboard.seconds
+		minutes.value = scoreboard.minutes
+    seconds.value = scoreboard.seconds
+		quarter.value = scoreboard.period
     // TODO: Quarter
     possession.value = scoreboard.possession
     home.score = scoreboard.home_score
